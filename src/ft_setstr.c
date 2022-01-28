e4756721a39070e8c27d6b7c1d0e8c40e6534954
mayocorn <stsunoda@student.42tokyo.jp>
2022-01-29 04:05:19 +0900

    modify

--- src/ft_setstr.c
+++ src/ft_setstr.c
@@ -6,7 +6,7 @@
 /*   By: stsunoda <stsunoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 02:08:41 by stsunoda          #+#    #+#             */
-/*   Updated: 2022/01/29 03:04:18 by stsunoda         ###   ########.fr       */
+/*   Updated: 2022/01/29 04:04:53 by stsunoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,42 @@
 
 static void	ft_setstr_minusflag(t_info *info, char *s)
 {
-	while (*(info->prefix) != '\0')
-		*(info->str++) = *(info->prefix++);
-	ft_memcpy(info->str, s, info->s_len);
-	ft_memset(info->str + info->s_len, ' ', info->space_len);
+	char	*str;
+	char	*prefix;
+
+	str = info->str;
+	prefix = info->prefix;
+	while (*prefix != '\0')
+		*(str++) = *(prefix++);
+	ft_memcpy(str, s, info->s_len);
+	ft_memset(str + info->s_len, ' ', info->space_len);
 }
 
 static void	ft_setstr_zeroflag(t_info *info, char *s)
 {
-	while (*(info->prefix) != '\0')
-		*(info->str++) = *(info->prefix++);
-	ft_memset(info->str, '0', info->space_len);
-	fr_memcpy(info->str + info->space_len, s, info->s_len);
+	char	*str;
+	char	*prefix;
+
+	str = info->str;
+	prefix = info->prefix;
+	while (*prefix != '\0')
+		*(str++) = *(prefix++);
+	ft_memset(str, '0', info->space_len);
+	ft_memcpy(str + info->space_len, s, info->s_len);
 }
 
 static void	ft_setstr_nonflag(t_info *info, char *s)
 {
-	ft_memset(info->str, ' ', info->space_len);
-	info->str += info->space_len;
-	while (*(info->prefix) != '\0')
-		*(info->str++) = *(info->prefix++);
-	ft_memcpy(info->str, s, info->s_len);
+	char	*str;
+	char	*prefix;
+
+	str = info->str;
+	prefix = info->prefix;
+	ft_memset(str, ' ', info->space_len);
+	str += info->space_len;
+	while (*prefix != '\0')
+		*(str++) = *(prefix++);
+	ft_memcpy(str, s, info->s_len);
 }
 
 void	ft_setstr(t_info *info, char *s)
